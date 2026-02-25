6e8b750c68be45d068e9f6d308a4ad468956c7ed
deepend <deepend@tilde.club>
2026-02-25 13:04:05 -0700

    Removed an obsolete commented-out hdiutil line from the macOS bundling script, leaving only the active packaging path (zip) to keep the script cleaner and easier to scan.

--- osx/makebundle.sh
+++ osx/makebundle.sh
@@ -6,6 +6,4 @@ rm -f *.app.zip
 python $HOME/.local/bin/gtk-mac-bundler zoitechat.bundle
 
 echo "Compressing bundle"
-#hdiutil create -format UDBZ -srcdir ZoiteChat.app -quiet ZoiteChat-2.9.6.1-$(git rev-parse --short master).dmg
 zip -9rXq ./ZoiteChat-$(git describe --tags).app.zip ./ZoiteChat.app
-
