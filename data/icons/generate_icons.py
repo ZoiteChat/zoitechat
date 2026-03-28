2e2eea4a461841e3b1e4427ad5ade9727a562557
deepend-tildeclub <deepend@tilde.club>
2026-03-28 00:53:20 -0600

    Drop magick probe from GTK Meson build

--- data/icons/generate_icons.py
+++ data/icons/generate_icons.py
@@ -106,7 +106,6 @@ def parse_args():
 
 def main():
     args = parse_args()
-    magick_cmd()
     svg = pathlib.Path(args.input_svg)
     out_png = pathlib.Path(args.output_png)
     out_png.parent.mkdir(parents=True, exist_ok=True)
